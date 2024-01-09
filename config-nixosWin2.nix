e97ab727fb428cb4e8e045f2954119780b02329b
Sean Methot <seanmethot@gmail.com>
2024-01-08 19:49:32 -0500

    Added brightnessctl to the GPDWin2 config

--- config-nixosWin2.nix
+++ config-nixosWin2.nix
@@ -33,7 +33,7 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [ ];
+  environment.systemPackages = with pkgs; [ brightnessctl ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
