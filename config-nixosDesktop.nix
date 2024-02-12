64ec8d60fd207aeccd8837810fe27914d5ffacca
Sean Methot <seanmethot@gmail.com>
2024-02-12 16:02:13 -0500

    Removed the NVidia junk - new GPU is AMD

--- config-nixosDesktop.nix
+++ config-nixosDesktop.nix
@@ -8,7 +8,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
+  services.xserver.videoDrivers = [ "amdgpu" ];
   networking.hostName = "nixosDesktop"; # Define your hostname.
   #networking.wireless.enable =
   # true; # Enables wireless support via wpa_supplicant.
@@ -36,38 +36,37 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [  ];
+  environment.systemPackages = with pkgs; [ ];
 
+  # services.xserver.videoDrivers = ["nvidia"];
 
-  services.xserver.videoDrivers = ["nvidia"];
+  # hardware.nvidia = {
 
-  hardware.nvidia = {
+  #   # Modesetting is required.
+  #   modesetting.enable = true;
 
-    # Modesetting is required.
-    modesetting.enable = true;
+  #   # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+  #   powerManagement.enable = false;
+  #   # Fine-grained power management. Turns off GPU when not in use.
+  #   # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+  #   powerManagement.finegrained = false;
 
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    powerManagement.enable = false;
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
+  #   # Use the NVidia open source kernel module (not to be confused with the
+  #   # independent third-party "nouveau" open source driver).
+  #   # Support is limited to the Turing and later architectures. Full list of
+  #   # supported GPUs is at:
+  #   # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+  #   # Only available from driver 515.43.04+
+  #   # Currently alpha-quality/buggy, so false is currently the recommended setting.
+  #   open = false;
 
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
+  #   # Enable the Nvidia settings menu,
+  #   # accessible via `nvidia-settings`.
+  #   nvidiaSettings = true;
 
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
+  #   # Optionally, you may need to select the appropriate driver version for your specific GPU.
+  #   package = config.boot.kernelPackages.nvidiaPackages.stable;
+  # };
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
