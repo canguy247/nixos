56ba12a280add890283bf0b2d5b54db1b904c08a
Sean Methot <seanmethot@gmail.com>
2024-04-03 14:52:59 -0400

    Tweaks for Alacritty

--- home.nix
+++ home.nix
@@ -71,7 +71,7 @@
     ".config/hypr/monitor-nixosBenchtop.conf".source =
       dotfiles/monitor-nixosBenchtop.conf;
 
-    ".config/alacritty/alacritty.toml".source = dotfiles/alacritty.toml;
+    #    ".config/alacritty/alacritty.toml".source = dotfiles/alacritty.toml;
 
     ".config/doom/config.el".source = dotfiles/config.el;
     ".config/doom/init.el".source = dotfiles/init.el;
@@ -169,7 +169,6 @@
         fi
       }
       e () {
-        cd /home/methots/.dotfiles
         if [ "$#" -eq 0  ]
         then
           emacs -nw
@@ -279,6 +278,52 @@
       symbol = "";
       style = "bg:#212736";
       format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+    };
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings = rec {
+      opacity = 0.9;
+      draw_bold_text_with_bright_colors = true;
+      font.size = 9.5;
+      colors = {
+        # Default colors
+        primary = {
+          background = "#000000";
+          foreground = "#c5c8c6";
+        };
+
+        # Colors the cursor will use if `custom_cursor_colors` is true
+        cursor = {
+          text = "#1d1f21";
+          cursor = "#ffffff";
+        };
+
+        # Normal colors
+        normal = {
+          black = "#1d1f21";
+          red = "#cc6666";
+          green = "#b5bd68";
+          yellow = "#e6c547";
+          blue = "#81a2be";
+          magenta = "#b294bb";
+          cyan = "#70c0ba";
+          white = "#373b41";
+        };
+
+        # Bright colors
+        bright = {
+          black = "#aaaaaa";
+          red = "#ff3334";
+          green = "#9ec400";
+          yellow = "#f0c674";
+          blue = "#81a2be";
+          magenta = "#b77ee0";
+          cyan = "#54ced6";
+          white = "#282a2e";
+        };
+      };
     };
   };
 
